ea38e00fd21725c9c19413ccdf4f2b15bf05c91e
Ashwini <ashie.oh@gmail.com>
2017-01-09 11:00:44 +0000

    chap 08 ex10

---
+++ chap08/ex10.rb
@@ -0,0 +1,75 @@
+# to remove newline character from a string - delete(), tr(), gsub(), strip(), lstrip(), rstrip()
+# always use double quoted when refering to newline character
+
+# singular_plural method used 4 times within other methods
+# make a list of existing cohorts in students array
+# display students grouped by cohorts
+# note: default cohort is november & if cohort entered has error, a random one will be assigned
+
+def input_students
+  puts "Please enter the names of the student"
+  name = gets.delete("\n")   # get the first name
+  puts "Please enter the cohort of the student"
+  cohort = gets.delete("\n") # get the cohort
+  students = [] # create an empty array
+  puts "To finish, just hit return when the student's name is asked"
+  while !name.empty? do  # while the name is not empty, repeat this code
+    cohort =  cohort_check(cohort) # november will be assigned if nothing entered
+    # add the student hash to the array
+    students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} student#{singular_plural(students)}"
+    puts "Please enter the next student's name"
+    name = gets.tr("\n", "")  # get another name from the user
+    break if name.empty? # get out of loop if name variable is empty
+    puts "Please enter the student's cohort"
+    cohort = gets.strip
+  end
+  #return the array of students
+  students
+end
+
+def singular_plural(students)
+  students.count > 1 ? "s" : ''
+end
+def cohort_check(cohort_name)
+  cohort_name = cohort_name.empty? ? "november" : cohort_name.downcase
+  # check that cohort_name is one of the 12 months
+  possible_name = %w(january feburary march april may june july august september october november december)
+  if possible_name.include? cohort_name
+    return cohort_name
+  else
+    return possible_name[rand(12)] # else send a random month
+  end
+end
+
+def existing_cohort(students) # list of unique cohort in students array
+  all_existing_cohorts = students.map do |student|
+    student[:cohort]
+  end.uniq
+end
+
+def print_header(students)
+  puts "The student#{singular_plural(students)} of Villains Academy"
+  puts "_" * 14
+end
+
+def print(students) # print students grouped by cohorts
+  all_existing_cohorts = existing_cohort(students)
+  all_existing_cohorts.each do |each_cohort|
+    one_cohort = students.map do |student| # list of students in one cohort
+      student[:name] if student[:cohort] == each_cohort
+    end.compact # to remove nil elements
+    puts "#{each_cohort.capitalize} cohort student#{singular_plural(one_cohort)}:"
+    puts one_cohort
+  end
+
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great student#{singular_plural(students)}"
+end
+#nothing happens until we call the methods
+students = input_students
+print_header(students)
+print(students)
+print_footer(students)
