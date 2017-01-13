f23912bdb593a98516a474df862ad3d86195ce26
Ashwini <ashie.oh@gmail.com>
2017-01-13 10:05:43 +0000

    added comments for explanation

--- chap14/ex8.rb
+++ chap14/ex8.rb
@@ -1,7 +1,7 @@
 # program that reads its own source code and prints it on the screen
 puts __FILE__ # name given after ruby. eg $ruby a/b/test.rb __FILE__ = a/b/test.rb
-puts File.dirname(__FILE__)
-puts File.basename(__FILE__)
+puts File.dirname(__FILE__) # directory of the given file
+puts File.basename(__FILE__) # name of the given file
 
 File.open(__FILE__,'r') do |f|
   f.readlines.each {|line| STDOUT.puts line}
